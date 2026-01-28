809c9fb30d812f202df3b2532563de5f08cdabf8
donovanmorrisonfritz-dot <donovanmorrisonfritz@gmail.com>
2026-01-28 15:09:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+
+yesBtn.addEventListener("click", () => {
+  response.textContent = "YAY!!! 💕 I knew you’d say yes 😍";
+  response.style.color = "#ff4d6d";
+  response.style.fontSize = "22px";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 300 - 150;
+  const y = Math.random() * 300 - 150;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
